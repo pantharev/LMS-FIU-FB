dcc696f29e908686bb1832fef94a152b5a9eb7cf
pantharev <narev001@fiu.edu>
2020-01-21 16:22:35 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello world!"});
+});
+
+require("./routes/student.routes")(app);
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+})
